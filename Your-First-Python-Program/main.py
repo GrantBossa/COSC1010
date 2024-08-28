c41d88c97e4f309b3507255bdbfd44cf4b4d4228
Grant A. Bossa <Grantbossa@westernwyoming.edu>
2024-08-28 10:17:36 -0600

    Update main.py

--- Your-First-Python-Program/main.py
+++ Your-First-Python-Program/main.py
@@ -1,8 +1,10 @@
 #
-# Name
-# Date
-# Your First Python Program
+# Grant Bossa
+# August 28, 2024
+# My First Python Program
 # COSC 1010 
 #
 
 # This is my first Python program.
+
+print("Hello World!")
