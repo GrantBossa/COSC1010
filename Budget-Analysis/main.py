bdf8d7e07d5aa489aa8ae52b883e748a7ba559d1
Grant A. Bossa <Grantbossa@westernwyoming.edu>
2024-09-18 10:42:40 -0600

    Update main.py

--- Budget-Analysis/main.py
+++ Budget-Analysis/main.py
@@ -1,7 +1,11 @@
 #
-# Name
-# Date
+# Grant Bossa
+# Sept 18, 2024
 # Budget Analysis Programming Project
-# COSC 2409 DNT
+# COSC 1010 NT
 #
 # Use comments liberally throughout the program. 
+# Write a program that asks the user to enter the amount that he or she has budgeted for a month. 
+# A loop should then prompt the user to enter each of his or her expenses for the month and keep a running total. 
+# When the loop finishes, the program should display the amount that the user is over or under budget.
+#
