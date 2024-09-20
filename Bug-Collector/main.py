680145f9ab0d5230a90bb60e8931baaebecd5b51
Grant A. Bossa <Grantbossa@westernwyoming.edu>
2024-09-20 01:34:05 -0600

    Finished Bug_Collector assignment

--- Bug-Collector/main.py
+++ Bug-Collector/main.py
@@ -1,8 +1,8 @@
 #
-# Name
-# Date
+# Grant Bossa
+# Sept 19, 2024
 # Bug Collector Programming Project
-# COSC 2409 DNT
+# COSC 1010 NT
 #
 # A bug collector collects bugs every day for five days. 
 # Write a program that keeps a running total of the number of bugs collected during the five days. 
@@ -10,10 +10,17 @@
 # when the loop is finished, the program should display the total number of bugs collected.
 #
 # Initialize variables for bugs and total number of bugs collected.
-total_running = 0
-total_each_day = 0
-counter_day = 0
+total_running = 0               # running total of bugs collected.
+bugs_collected_daily = 0    # total number of bugs collected.
+counter_day = 0                 # counter for the day.
 
 # Get number of bugs collected each day using a for loop.
+for counter_day in range(5):
+    print(f"Day {counter_day + 1}:", end=" " * 5)
+    bugs_collected_daily = int(input("Enter the number of bugs collected: "))
+    total_running +=  bugs_collected_daily
 
 # Display the total number of bugs collected.
+print(f"Total number of bugs collected over the five days: {total_running}")
+
+# End of program.
