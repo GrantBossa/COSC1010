bac6976ca24d619be007703e851d823f2d7d5bdd
Grant A. Bossa <Grantbossa@westernwyoming.edu>
2024-09-24 15:58:01 -0600

    Update kilometer_converter.py

--- Kilometer-Converter/kilometer_converter.py
+++ Kilometer-Converter/kilometer_converter.py
@@ -1,7 +1,15 @@
 #
-# Name
-# Date
+# Grant Bossa  
+# September 24, 2024
 # Kilometer Converter Programming Project
 # COSC 2409 DNT
 #
-# Use comments liberally throughout the program. 
+# Instructions
+# Write a program that asks the user to enter a distance in kilometers, 
+# then converts that distance to miles. The conversion formula is as follows: Miles = Kilometers x 0.6214
+#
+# Declare Variables
+
+# asks the user to enter a distance in kilometers
+
+# converts that distance to miles
