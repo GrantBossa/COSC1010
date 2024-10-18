56a8c00ab7e11d7184530765d5ef747f636ece10
Grant A. Bossa <Grantbossa@westernwyoming.edu>
2024-10-17 22:11:07 -0600

    Update for Average of numbers project

--- Average-of-Numbers/main.py
+++ Average-of-Numbers/main.py
@@ -1,7 +1,33 @@
 #
-# Name
-# Date
+# Grant Bossa
+# October 15, 2024
 # Average of Numbers Programming Project
-# COSC 2409 DNT
+# COSC 1010
 #
-# Use comments liberally throughout the program. 
+# Instructions
+# Assume a file containing a series of integers is named `numbers.txt` and exists on the computer’s disk. 
+# Write a program that calculates the average of all the numbers stored in the file.   
+#
+# Define Variables
+number = 0
+counter = 0
+sum = 0
+average = 0
+
+# Open file
+myfile = open("numbers.txt", "r")
+# Read file
+for line in myfile:
+    number = int(line)
+    sum += number
+    counter += 1
+    average =sum/counter
+    # Display file contents
+    print(number)
+print('Numbers found',counter, '\nSum = ' , sum , '\nAverage = ' , average )
+
+
+# Close file
+myfile.close()
+
+# End Program
