6d5c9de9ea58580dbd0c769943cccd8806fa5116
Grant A. Bossa <Grantbossa@westernwyoming.edu>
2024-10-17 22:12:52 -0600

    Update for File Display

--- File-Display/main.py
+++ File-Display/main.py
@@ -1,7 +1,23 @@
 #
-# Name
-# Date
+# Grant Bossa
+# October 15, 2024
 # File Display Programming Project
-# COSC 2409 DNT
+# COSC 1010
 #
 # Use comments liberally throughout the program. 
+#
+# Define Variables
+number = 0
+
+# Open file
+myfile = open("numbers.txt", 'r')
+# Read file
+for line in myfile:
+    number = int(line)
+    # Display file contents
+    print(number)
+
+# Close file
+myfile.close()
+
+# End Program
